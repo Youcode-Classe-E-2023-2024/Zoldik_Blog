0db907cdc50870628b88f589aea04330a0961e77
Naoufal <naoufal.labrihmi@gmail.com>
2023-12-08 12:02:36 +0100

    add categories file

--- blog.sql
+++ blog.sql
@@ -1,31 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 04, 2023 at 04:53 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `blog`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `articles`
---
 
 CREATE TABLE `articles` (
   `id` int(11) UNSIGNED NOT NULL,
@@ -86,6 +64,7 @@ CREATE TABLE `users` (
 
 
 
+
 --
 -- Dumping data for table `users`
 --
@@ -139,6 +118,4 @@ ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
